af99fb8978d58f894bfe4f735fe8741c0158373f
Erik Wiberg <erik.wiberg.87@gmail.com>
2015-10-01 16:26:30 +0200

    ListHelper updates, not finished

--- HelperMethods/src/helpermethods/collections/ListHelper.java
+++ HelperMethods/src/helpermethods/collections/ListHelper.java
@@ -1,4 +1,5 @@
 package helpermethods.collections;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,15 +18,44 @@ public class ListHelper {
 		return subList;
 	}
 	
-	public <T, U extends T> List<U> getSubList(List<T> list, Class<U> classToExtract){
+	
+	/**
+	 * Given a list of type T, return a list of only the elements that match the sub class (U extends T)
+	 * @param list
+	 * @param classToMatch
+	 * @return
+	 */
+	public <T, U extends T> List<U> getSubList(List<T> list, Class<U> classToMatch){
 		
 		List<U> subList = new ArrayList<U>();
 		
 		for(T t : list){
-			if(classToExtract.isInstance(t)){
+			if(classToMatch.isInstance(t)){
 				subList.add((U) t);
 			}
 		}
 		return subList;
 	}
+	
+	/**
+	 * Given a list (instance of X) of type T, return a list (instance of X) of only the elements that match the sub class (U extends T)
+	 * @param list
+	 * @param classToMatch
+	 * @return
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 */
+	public <T, U extends T> List<U> getSubListOfSameListType(List<T> list, Class<U> classToMatch) throws InstantiationException, IllegalAccessException{
+		
+		Class listType = list.getClass();
+		List<U> subList = (List<U>) listType.newInstance();
+		
+		for(T t : list){
+			if(classToMatch.isInstance(t)){
+				subList.add((U) t);
+			}
+		}
+		return subList;
+	}
+	
 }
